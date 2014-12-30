b62dde698651571fa80c90cfac47539e7f72e015
Takashi Toyoshima <toyoshim@google.com>
2014-12-31 02:34:54 +0900

    Update RAM_64Kx8 simulation model so to have delay parameter

--- ram/simulation/RAM_64Kx8.v
+++ ram/simulation/RAM_64Kx8.v
@@ -8,13 +8,15 @@ module RAM_64Kx8(
     i_write_x,
     i_data,
     o_data);
+  parameter delay = 10;
   input  [15:0] i_addr;
   input         i_enable_x;
   input         i_write_x;
   input  [ 7:0] i_data;
   output [ 7:0] o_data;
 
-  RAM #(.width(8),
+  RAM #(.delay(delay),
+        .width(8),
         .depth(16)) ram(
       .i_addr    (i_addr    ),
       .i_enable_x(i_enable_x),
